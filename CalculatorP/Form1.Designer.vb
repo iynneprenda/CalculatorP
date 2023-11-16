8487bdb6303aff746d5383b65275b27bd3b872f0
iynne <iynne@DESKTOP-JDF9FR0>
2023-11-17 01:46:10 +0800

    Add project files.

---
+++ CalculatorP/Form1.Designer.vb
@@ -0,0 +1,337 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(Form1))
+        Number0 = New Button()
+        Point = New Button()
+        Equal = New Button()
+        Addition = New Button()
+        Number3 = New Button()
+        Number2 = New Button()
+        Number1 = New Button()
+        Subtraction = New Button()
+        Number6 = New Button()
+        Number5 = New Button()
+        Number4 = New Button()
+        Multiplication = New Button()
+        Number9 = New Button()
+        Number8 = New Button()
+        Number7 = New Button()
+        Division = New Button()
+        cancel1 = New Button()
+        cancel2 = New Button()
+        percent = New Button()
+        Erasebutton = New Button()
+        Label = New Label()
+        Label1 = New Label()
+        Label2 = New Label()
+        SuspendLayout()
+        ' 
+        ' Number0
+        ' 
+        Number0.Font = New Font("Segoe MDL2 Assets", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Number0.Location = New Point(1, 441)
+        Number0.Name = "Number0"
+        Number0.Size = New Size(75, 52)
+        Number0.TabIndex = 1
+        Number0.Text = "0"
+        Number0.UseVisualStyleBackColor = True
+        ' 
+        ' Point
+        ' 
+        Point.Font = New Font("Segoe MDL2 Assets", 18F, FontStyle.Regular, GraphicsUnit.Point)
+        Point.Location = New Point(76, 441)
+        Point.Name = "Point"
+        Point.Size = New Size(75, 52)
+        Point.TabIndex = 2
+        Point.Text = "."
+        Point.UseVisualStyleBackColor = True
+        ' 
+        ' Equal
+        ' 
+        Equal.BackColor = SystemColors.Highlight
+        Equal.Font = New Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point)
+        Equal.ForeColor = SystemColors.ButtonHighlight
+        Equal.Location = New Point(151, 441)
+        Equal.Name = "Equal"
+        Equal.Size = New Size(151, 52)
+        Equal.TabIndex = 3
+        Equal.Text = "="
+        Equal.UseVisualStyleBackColor = False
+        ' 
+        ' Addition
+        ' 
+        Addition.Font = New Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point)
+        Addition.Location = New Point(227, 389)
+        Addition.Name = "Addition"
+        Addition.Size = New Size(75, 52)
+        Addition.TabIndex = 7
+        Addition.Text = "+"
+        Addition.UseVisualStyleBackColor = True
+        ' 
+        ' Number3
+        ' 
+        Number3.Font = New Font("Segoe MDL2 Assets", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Number3.Location = New Point(151, 389)
+        Number3.Name = "Number3"
+        Number3.Size = New Size(75, 52)
+        Number3.TabIndex = 6
+        Number3.Text = "3"
+        Number3.UseVisualStyleBackColor = True
+        ' 
+        ' Number2
+        ' 
+        Number2.Font = New Font("Segoe MDL2 Assets", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Number2.Location = New Point(76, 389)
+        Number2.Name = "Number2"
+        Number2.Size = New Size(75, 52)
+        Number2.TabIndex = 5
+        Number2.Text = "2"
+        Number2.UseVisualStyleBackColor = True
+        ' 
+        ' Number1
+        ' 
+        Number1.Font = New Font("Segoe MDL2 Assets", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Number1.Location = New Point(1, 389)
+        Number1.Name = "Number1"
+        Number1.Size = New Size(75, 52)
+        Number1.TabIndex = 4
+        Number1.Text = "1"
+        Number1.UseVisualStyleBackColor = True
+        ' 
+        ' Subtraction
+        ' 
+        Subtraction.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point)
+        Subtraction.Location = New Point(227, 337)
+        Subtraction.Name = "Subtraction"
+        Subtraction.Size = New Size(75, 52)
+        Subtraction.TabIndex = 11
+        Subtraction.Text = "-"
+        Subtraction.UseVisualStyleBackColor = True
+        ' 
+        ' Number6
+        ' 
+        Number6.Font = New Font("Segoe MDL2 Assets", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Number6.Location = New Point(151, 337)
+        Number6.Name = "Number6"
+        Number6.Size = New Size(75, 52)
+        Number6.TabIndex = 10
+        Number6.Text = "6"
+        Number6.UseVisualStyleBackColor = True
+        ' 
+        ' Number5
+        ' 
+        Number5.Font = New Font("Segoe MDL2 Assets", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Number5.Location = New Point(76, 337)
+        Number5.Name = "Number5"
+        Number5.Size = New Size(75, 52)
+        Number5.TabIndex = 9
+        Number5.Text = "5"
+        Number5.UseVisualStyleBackColor = True
+        ' 
+        ' Number4
+        ' 
+        Number4.Font = New Font("Segoe MDL2 Assets", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Number4.Location = New Point(1, 337)
+        Number4.Name = "Number4"
+        Number4.Size = New Size(75, 52)
+        Number4.TabIndex = 8
+        Number4.Text = "4"
+        Number4.UseVisualStyleBackColor = True
+        ' 
+        ' Multiplication
+        ' 
+        Multiplication.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Multiplication.Location = New Point(227, 285)
+        Multiplication.Name = "Multiplication"
+        Multiplication.Size = New Size(75, 52)
+        Multiplication.TabIndex = 15
+        Multiplication.Text = "x"
+        Multiplication.UseVisualStyleBackColor = True
+        ' 
+        ' Number9
+        ' 
+        Number9.Font = New Font("Segoe MDL2 Assets", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Number9.Location = New Point(151, 285)
+        Number9.Name = "Number9"
+        Number9.Size = New Size(75, 52)
+        Number9.TabIndex = 14
+        Number9.Text = "9"
+        Number9.UseVisualStyleBackColor = True
+        ' 
+        ' Number8
+        ' 
+        Number8.Font = New Font("Segoe MDL2 Assets", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Number8.Location = New Point(76, 285)
+        Number8.Name = "Number8"
+        Number8.Size = New Size(75, 52)
+        Number8.TabIndex = 13
+        Number8.Text = "8"
+        Number8.UseVisualStyleBackColor = True
+        ' 
+        ' Number7
+        ' 
+        Number7.Font = New Font("Segoe MDL2 Assets", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Number7.Location = New Point(1, 285)
+        Number7.Name = "Number7"
+        Number7.Size = New Size(75, 52)
+        Number7.TabIndex = 12
+        Number7.Text = "7"
+        Number7.UseVisualStyleBackColor = True
+        ' 
+        ' Division
+        ' 
+        Division.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point)
+        Division.Location = New Point(227, 233)
+        Division.Name = "Division"
+        Division.Size = New Size(75, 52)
+        Division.TabIndex = 19
+        Division.Text = "÷"
+        Division.UseVisualStyleBackColor = True
+        ' 
+        ' cancel1
+        ' 
+        cancel1.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        cancel1.Location = New Point(151, 233)
+        cancel1.Name = "cancel1"
+        cancel1.Size = New Size(75, 52)
+        cancel1.TabIndex = 18
+        cancel1.Text = "C"
+        cancel1.UseVisualStyleBackColor = True
+        ' 
+        ' cancel2
+        ' 
+        cancel2.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        cancel2.Location = New Point(76, 233)
+        cancel2.Name = "cancel2"
+        cancel2.Size = New Size(75, 52)
+        cancel2.TabIndex = 17
+        cancel2.Text = "CE"
+        cancel2.UseVisualStyleBackColor = True
+        ' 
+        ' percent
+        ' 
+        percent.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        percent.Location = New Point(1, 233)
+        percent.Name = "percent"
+        percent.Size = New Size(75, 52)
+        percent.TabIndex = 16
+        percent.Text = "%"
+        percent.UseVisualStyleBackColor = True
+        ' 
+        ' Erasebutton
+        ' 
+        Erasebutton.Font = New Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point)
+        Erasebutton.Location = New Point(227, 181)
+        Erasebutton.Name = "Erasebutton"
+        Erasebutton.Size = New Size(75, 52)
+        Erasebutton.TabIndex = 20
+        Erasebutton.Text = "⌫"
+        Erasebutton.UseVisualStyleBackColor = True
+        ' 
+        ' Label
+        ' 
+        Label.Font = New Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point)
+        Label.Location = New Point(6, 119)
+        Label.Name = "Label"
+        Label.Size = New Size(290, 60)
+        Label.TabIndex = 21
+        Label.Text = "0"
+        Label.TextAlign = ContentAlignment.BottomRight
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(255, 104)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(0, 15)
+        Label1.TabIndex = 22
+        Label1.TextAlign = ContentAlignment.BottomRight
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(255, 104)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(0, 15)
+        Label2.TabIndex = 23
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(303, 494)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(Label)
+        Controls.Add(Erasebutton)
+        Controls.Add(Division)
+        Controls.Add(cancel1)
+        Controls.Add(cancel2)
+        Controls.Add(percent)
+        Controls.Add(Multiplication)
+        Controls.Add(Number9)
+        Controls.Add(Number8)
+        Controls.Add(Number7)
+        Controls.Add(Subtraction)
+        Controls.Add(Number6)
+        Controls.Add(Number5)
+        Controls.Add(Number4)
+        Controls.Add(Addition)
+        Controls.Add(Number3)
+        Controls.Add(Number2)
+        Controls.Add(Number1)
+        Controls.Add(Equal)
+        Controls.Add(Point)
+        Controls.Add(Number0)
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        Name = "Form1"
+        Text = "Calculator"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+    Friend WithEvents Number0 As Button
+    Friend WithEvents Point As Button
+    Friend WithEvents Equal As Button
+    Friend WithEvents Addition As Button
+    Friend WithEvents Number3 As Button
+    Friend WithEvents Number2 As Button
+    Friend WithEvents Number1 As Button
+    Friend WithEvents Subtraction As Button
+    Friend WithEvents Number6 As Button
+    Friend WithEvents Number5 As Button
+    Friend WithEvents Number4 As Button
+    Friend WithEvents Multiplication As Button
+    Friend WithEvents Number9 As Button
+    Friend WithEvents Number8 As Button
+    Friend WithEvents Number7 As Button
+    Friend WithEvents Division As Button
+    Friend WithEvents cancel1 As Button
+    Friend WithEvents cancel2 As Button
+    Friend WithEvents percent As Button
+    Friend WithEvents Erasebutton As Button
+    Friend WithEvents Label As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ CalculatorP/Form1.vb
@@ -0,0 +1,190 @@
+﻿Public Class Form1
+
+    Dim firstnum As Decimal
+    Dim secondnum As Decimal
+    Dim operation As Integer
+    Dim Operator_selector As Boolean = False
+
+    Private Sub Label_Click(sender As Object, e As EventArgs) Handles Label.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Number0_Click(sender As Object, e As EventArgs) Handles Number0.Click
+        If Label.Text <> "0" Then
+            Label.Text += "0"
+        End If
+    End Sub
+
+    Private Sub Point_Click(sender As Object, e As EventArgs) Handles Point.Click
+        If Not (Label.Text.Contains(".")) Then
+            Label.Text += "."
+        End If
+    End Sub
+
+    Private Sub Equal_Click(sender As Object, e As EventArgs) Handles Equal.Click
+        Label2.Text = Label2.Text + Label.Text
+        Label2.Text = Label2.Text + " ="
+
+        If Operator_selector = True Then
+            secondnum = Label.Text
+
+            If operation = 1 Then
+                Label.Text = firstnum + secondnum
+            ElseIf operation = 2 Then
+                Label.Text = firstnum - secondnum
+            ElseIf operation = 3 Then
+                Label.Text = firstnum * secondnum
+            Else
+                If secondnum = 0 Then
+                    Label.Text = "Error"
+                Else
+                    Label.Text = firstnum / secondnum
+                End If
+            End If
+            Operator_selector = False
+        End If
+    End Sub
+
+    Private Sub Number1_Click(sender As Object, e As EventArgs) Handles Number1.Click
+        If Label.Text <> "0" Then
+            Label.Text += "1"
+        Else
+            Label.Text = "1"
+
+        End If
+
+    End Sub
+
+    Private Sub Number2_Click(sender As Object, e As EventArgs) Handles Number2.Click
+        If Label.Text <> "0" Then
+            Label.Text += "2"
+        Else
+            Label.Text = "2"
+
+        End If
+    End Sub
+
+    Private Sub Number3_Click(sender As Object, e As EventArgs) Handles Number3.Click
+        If Label.Text <> "0" Then
+            Label.Text += "3"
+        Else
+            Label.Text = "3"
+
+        End If
+    End Sub
+
+    Private Sub Number4_Click(sender As Object, e As EventArgs) Handles Number4.Click
+        If Label.Text <> "0" Then
+            Label.Text += "4"
+        Else
+            Label.Text = "4"
+
+        End If
+    End Sub
+
+    Private Sub Number5_Click(sender As Object, e As EventArgs) Handles Number5.Click
+        If Label.Text <> "0" Then
+            Label.Text += "5"
+        Else
+            Label.Text = "5"
+
+        End If
+    End Sub
+
+    Private Sub Number6_Click(sender As Object, e As EventArgs) Handles Number6.Click
+        If Label.Text <> "0" Then
+            Label.Text += "6"
+        Else
+            Label.Text = "6"
+
+        End If
+    End Sub
+
+    Private Sub Number7_Click(sender As Object, e As EventArgs) Handles Number7.Click
+        If Label.Text <> "0" Then
+            Label.Text += "7"
+        Else
+            Label.Text = "7"
+
+        End If
+    End Sub
+
+    Private Sub Number8_Click(sender As Object, e As EventArgs) Handles Number8.Click
+        If Label.Text <> "0" Then
+            Label.Text += "8"
+        Else
+            Label.Text = "8"
+
+        End If
+    End Sub
+
+    Private Sub Number9_Click(sender As Object, e As EventArgs) Handles Number9.Click
+        If Label.Text <> "0" Then
+            Label.Text += "9"
+        Else
+            Label.Text = "9"
+
+        End If
+    End Sub
+
+    Private Sub Addition_Click(sender As Object, e As EventArgs) Handles Addition.Click
+        Label2.Text = Label2.Text + Label.Text
+        Label2.Text = Label2.Text + " +"
+        firstnum = Label.Text
+        Label.Text = "0"
+        Operator_selector = True
+        operation = 1 ' = +
+    End Sub
+
+    Private Sub Subtraction_Click(sender As Object, e As EventArgs) Handles Subtraction.Click
+        Label2.Text = Label2.Text + Label.Text
+        Label2.Text = Label2.Text + " -"
+        firstnum = Label.Text
+        Label.Text = "0"
+        Operator_selector = True
+        operation = 2 ' = -
+    End Sub
+
+    Private Sub Multiplication_Click(sender As Object, e As EventArgs) Handles Multiplication.Click
+        Label2.Text = Label2.Text + Label.Text
+        Label2.Text = Label2.Text + " x"
+        firstnum = Label.Text
+        Label.Text = "0"
+        Operator_selector = True
+        operation = 3 ' = x
+    End Sub
+
+    Private Sub Division_Click(sender As Object, e As EventArgs) Handles Division.Click
+        Label2.Text = Label2.Text + Label.Text
+        Label2.Text = Label2.Text + " ÷"
+        firstnum = Label.Text
+        Label.Text = "0"
+        Operator_selector = True
+        operation = 4 ' = ÷
+    End Sub
+
+    Private Sub Erasebutton_Click(sender As Object, e As EventArgs) Handles Erasebutton.Click
+
+    End Sub
+
+    Private Sub cancel1_Click(sender As Object, e As EventArgs) Handles cancel1.Click
+
+
+    End Sub
+
+    Private Sub cancel2_Click(sender As Object, e As EventArgs) Handles cancel2.Click
+
+    End Sub
+
+    Private Sub percent_Click(sender As Object, e As EventArgs) Handles percent.Click
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+End Class
